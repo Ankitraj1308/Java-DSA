a669dda0aba1c9e9d21b949f1b006ec03435acc7
Ankit Raj <ankit13082005raj@gmail.com>
2026-09-24 18:41:33 +0530

    started array

---
+++ Array/java.java
@@ -0,0 +1,28 @@
+public class java{
+    static int[] rotatingArray(int[] a, int k){
+        int n = a.length;
+        k = k%n;
+        int j=0;
+        int[] ans = new int[n];
+        for(int i=n-k;i<n;i++){
+            ans[j++]=a[i];
+        }
+        for(int i=0;i<n-k;i++){
+            ans[j++]=a[i];
+        }
+        return ans;
+
+    }
+
+public static void main(String[] args){
+    int[] a = {1,2,3,4,5,6,7};
+    int[] rev = rotatingArray(a,5);
+    for (int i = 0; i < rev.length; i++) {
+        System.out.print(rev[i]);
+    }
+    System.out.println();
+}
+
+
+
+}
